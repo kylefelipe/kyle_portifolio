8a459ad101fff47533f1eb3d7c755ab0ea2dc918
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-12 21:33:18 -0300

    Resolução do exercício 05.

---
+++ exercises/4_1/exercicio_05.js
@@ -0,0 +1,13 @@
+let angulo1, angulo2, angulo3;
+
+angulo1 = 30;
+angulo2 = 60;
+angulo3 = 90;
+let isTriangulo = (angulo1 + angulo2 + angulo3) === 180
+
+
+if (isTriangulo == true) {
+    console.log(isTriangulo);
+} else {
+    console.log("Não não é um triângulo!");
+}
