a7e3c82188ec06a7682db45a822137b6a59adb5f
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-16 16:52:39 -0300

    Corrigindo indentação.

--- exercises/4_4/exercicio_01_pt2.js
+++ exercises/4_4/exercicio_01_pt2.js
@@ -2,13 +2,13 @@
 // Verificar se é Palíndromo
 
 function ePalindromo(palavra) {
-    let reverse = ""
-    for (let letra = palavra.length - 1; letra>=0 ; letra-=1) {
-        reverse += palavra[letra];
-    }
-    if (reverse === palavra) {
-        return true;
-    } else {return false};
+  let reverse = ""
+  for (let letra = palavra.length - 1; letra >= 0; letra -= 1) {
+    reverse += palavra[letra];
+  }
+  if (reverse === palavra) {
+    return true;
+  } else { return false };
 }
 
 console.log(ePalindromo('anp'));
--- exercises/4_4/exercicio_03_pt2.js
+++ exercises/4_4/exercicio_03_pt2.js
@@ -2,13 +2,13 @@
 // retornar o indice do menor valor
 
 function retornaIndiceMenorValor(vetor) {
-    let menor = 0;
-    for (let indice = 0; indice < vetor.length; indice+=1) {
-            if (vetor[menor] > vetor[indice]){
-                menor = indice;
-            }
-        }
-    return menor;
+  let menor = 0;
+  for (let indice = 0; indice < vetor.length; indice += 1) {
+    if (vetor[menor] > vetor[indice]) {
+      menor = indice;
+    }
+  }
+  return menor;
 }
 
 console.log(retornaIndiceMenorValor([2, 4, 6, 7, 10, 0, -3]))
