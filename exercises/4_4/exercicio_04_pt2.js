813b746846ff2e0c18eca4dd4327f4b7da6c17d1
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-16 16:50:59 -0300

    Corrigindo indentação.

--- exercises/4_4/exercicio_04_pt2.js
+++ exercises/4_4/exercicio_04_pt2.js
@@ -2,13 +2,13 @@
 let arrayTeste = ['José', 'Ana Marina', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
 function maiorNome(vetor) {
-    let maior = 0;
-    for (let indice in vetor) {
-        if (vetor[indice].length > vetor[maior].length) {
-            maior = indice;
-        }
+  let maior = 0;
+  for (let indice in vetor) {
+    if (vetor[indice].length > vetor[maior].length) {
+      maior = indice;
     }
-    return vetor[maior];
+  }
+  return vetor[maior];
 }
 
 console.log(maiorNome(arrayTeste));
