faeddca0a0640a9ee017ff72bdb34fdbf84acac8
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-06-08 15:01:55 -0300

    não sei o que eu fiz aqui.

--- exercises/7_1/execicio2.js
+++ exercises/7_1/execicio2.js
@@ -1,7 +1,16 @@
 // contar palavras
 
-const wordCount = (phrase) => phrase.split(' ').length;
+const longestWord = (str) => {
+  str = str.split(' ');
+  let bigWordId = 0;
+  for (let wordId = bigWordId; wordId < str.length; wordId += 1) {
+    if (str[wordId].length > str[bigWordId].length) bigWordId = wordId;
+  }
+  return str[bigWordId];
+}
 
 
-const phrase = 'socorram me subi no onibus em marrocos'
-console.log(wordCount(phrase))
+const phrase = 'me subi no onibus em marrocos'
+console.log(longestWord(phrase))
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+
