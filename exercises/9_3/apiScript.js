c58c904377214d36a4f73ad3c12bd5fcc70debb9
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-06-26 16:29:20 -0300

    Criando o JavaScript básico para o exercício.

---
+++ exercises/9_3/apiScript.js
@@ -0,0 +1,14 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+
+}
+
+const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' };
+}
+
+fetch(API_URL, myObject);
+
+windows.onload = () => fetchJoke();
