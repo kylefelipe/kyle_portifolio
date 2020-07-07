39a6c178aa8b15c5d12ae0ef73010ec3fcedbe1c
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-06 22:27:32 -0300

    Criando as 3 funções pedidas no exercício 04.

--- exercises/10_2/source_code2.js
+++ exercises/10_2/source_code2.js
@@ -1,5 +1,11 @@
-// const highBox = string => string.toUpperCase();
-// const firstCharacter = string => string[0];
+const highBox = string => string.toUpperCase();
+const firstCharacter = string => string[0];
+function concatStr(...params) {
+  return concat = params.reduce((acc, string) => acc + string)
+};
 
 module.exports = {
+  highBox,
+  firstCharacter,
+  concatStr,
 }
