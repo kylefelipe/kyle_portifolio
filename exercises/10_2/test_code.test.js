b5e4e3d827d4109c0e4a4b529bc377d52fa0ea82
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-06 23:47:46 -0300

    Depois de entender como era para fazer o exercício 4, montando os testes usando o jest.mock().

--- exercises/10_2/test_code.test.js
+++ exercises/10_2/test_code.test.js
@@ -33,23 +33,25 @@ describe('Testando a primeira função do exercício!', () => {
 });
 
 const srcCode2 = require('./source_code2');
+jest.mock('./source_code2');
 
-describe('Criar um novo arquivo (módulo) e criar 3 funções, antes do mocking!', () => {
+describe('Criar um novo arquivo (módulo) e criar 3 funções, e testar antes e depois de mockar!', () => {
   it('Deve haver 3 funções!', () => {
     const items = Object.keys(srcCode2);
     const isFunction = items.map(item => typeof srcCode2[item] === 'function').filter(item => item === true);
     expect(isFunction.length).toBe(3);
-    });
-  it('A primeira função deve receber uma string e retornar em caixa alta!', () => {
-    expect(srcCode2.highBox(usarNosTestes.texto)).toBe(usarNosTestes.texto.toUpperCase());
   });
-  it('A segunda função deve receber uma string e retornar apenas a primeira letra!', () => {
-    expect(srcCode2.firstCharacter(usarNosTestes.texto)).toBe(usarNosTestes.texto[0]);
+  it(`A primeira função deve receber uma string e retornar em caixa alta, antes do mock,
+  depois do mock deve retornar uma string em caixa baixa!`, () => {
+    srcCode2.highBox.mockImplementation(string => string.toLowerCase());
+    expect(srcCode2.highBox(usarNosTestes.texto)).toBe(usarNosTestes.texto.toLowerCase());
+  });
+  it('A segunda função deve receber uma string e retornar apenas a última letra!', () => {
+    srcCode2.firstCharacter.mockImplementation(string => string[-1]);
+    expect(srcCode2.firstCharacter(usarNosTestes.texto)).toBe(usarNosTestes.texto[-1]);
   });
   it('A terceira função deve receber duas strings e retornar sua concatenação!', () => {
+    srcCode2.concatStr.mockImplementation((...params) => params.reduce((acc, string) => acc + string));
     expect(srcCode2.concatStr(usarNosTestes.texto, usarNosTestes.outroTexto)).toBe(`${usarNosTestes.texto}${usarNosTestes.outroTexto}`);
   });
-});
-
-describe('Fazer o mocking do módulo criado anteriormente!', () => {
 });
