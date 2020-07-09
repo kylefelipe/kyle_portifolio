070d4891457d3279d139edf2a9ecea3e7a4a94aa
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-09 04:39:35 -0300

    Modificando umas coisas no exercício.

--- exercises/11_1/oi-mundo/src/App.js
+++ exercises/11_1/oi-mundo/src/App.js
@@ -15,9 +15,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         {taskList.map(task)}
         <a
           className="App-link"
